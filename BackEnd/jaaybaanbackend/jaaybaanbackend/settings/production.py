90eacf0e875358f7a4f62568a32246a0dabe96c0
mahdi <mahdiabbasi602@gmail.com>
2025-09-14 16:51:39 +0330

    fix: fix local ip env

--- BackEnd/jaaybaanbackend/jaaybaanbackend/settings/production.py
+++ BackEnd/jaaybaanbackend/jaaybaanbackend/settings/production.py
@@ -43,9 +43,7 @@ MEDIA_ROOT = "/app/media"
 
 CORS_ALLOW_ALL_ORIGINS = False
 # CORS settings for local network access
-CORS_ALLOWED_ORIGINS = config(
-    "CORS_ALLOWED_ORIGINS", default="http://localhost:8000"
-).split(",")
+CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS").split(",")
 print(f"CORS_ALLOWED_ORIGINS: {CORS_ALLOWED_ORIGINS}")
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_HEADERS = [
